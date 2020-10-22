d9e385e3f6a21eaa1a53187853ab4cf6629a1213
Tobias Mächler <tobias.maechler@yonder.info>
2020-10-22 17:28:14 +0200

    Schema, logischesDBModell und data erstellt

---
+++ Testat2/data.sql
@@ -0,0 +1,35 @@
+/* 
+ * Daten einfüllen
+ */
+
+INSERT INTO spieler (id, vorname, nachname, chipzahl, aktiv) VALUES
+    -- Profis
+    (1, 'Daniel', 'Negreanu', 100000, true),
+    (2, 'Phil', 'Ivey', 100000, true),
+    (3, 'Bryn', 'Kenney', 100000, true),
+    (4, 'Fedor', 'Holz', 100000, true),
+    (5, 'Antonio', 'Esfandiari', 100000, true),
+    (6, 'Vanessa', 'Selbst', 100000, true),
+    -- Beginner
+    (7, 'Ole', 'Schemion', 100000, true),
+    (8, 'Ioana', 'Dickerson', 100000, true),
+    (9, 'Alisha', 'Tillman', 100000, true),
+    (10, 'Maise', 'Burke', 100000, true),
+    (11, 'Peter', 'Gray', 100000, true),
+    (12, 'Issac', 'Myers', 100000, true),
+    (13, 'Jaidon', 'Mill', 100000, true),
+    (14, 'Hussain', 'Gale', 100000, true),
+    (15, 'Jimmy', 'Blevins', 100000, true);
+
+INSERT INTO TABLE floorman (id, vorname, nachname, erfahrung) VALUES
+    (1, 'James', 'Hartigan', 9),
+    (1, 'Joe', 'Stapleton', 9),
+    (1, 'Kara', 'Scott', 9),
+    (1, 'Lon', 'McEachern', 9),
+    (1, 'Maria', 'Ho', 9);
+
+INSERT INTO TABLE tournament (id, maxspieler, preisgeld, datum, ort, floorman) VALUES
+    ()
+
+INSERT INTO TABLE profi (spielerid, rank, tournament) VALUES
+    (1, 10, 1)
---
+++ Testat2/schema.sql
@@ -0,0 +1,52 @@
+/*
+ * Tabellen erzeugen
+ */
+
+
+CREATE TABLE spieler (
+    id INTEGER PRIMARY KEY,
+    vorname VARCHAR(50) NOT NULL,
+    nachname VARCHAR(50) NOT NULL,
+    chipzahl INTEGER NOT NULL,
+    aktiv BOOLEAN
+);
+
+
+CREATE TABLE floorman (
+    id INTEGER PRIMARY KEY,
+    vorname VARCHAR(50) NOT NULL,
+    nachname VARCHAR(50) NOT NULL,
+    erfahrung INTEGER NOT NULL
+);
+
+CREATE TABLE tournament (
+    id INTEGER PRIMARY KEY,
+    maxspieler INTEGER NOT NULL,
+    preisgeld INTEGER NOT NULL,
+    datum DATE NOT NULL,
+    ort VARCHAR(80) NOT NULL,
+    floorman INTEGER NOT NULL REFERENCES floorman(id)
+);
+
+CREATE TABLE profi (
+    spielerid INTEGER NOT NULL REFERENCES spieler(id),
+    rank INTEGER NOT NULL,
+    tournament INTEGER NOT NULL REFERENCES tournament(id)
+);
+
+CREATE TABLE qualifying (
+    id INTEGER PRIMARY KEY,
+    startdatum DATE NOT NULL,
+    endedatum DATE NOT NULL
+);
+
+CREATE TABLE beginner (
+    spielerid INTEGER NOT NULL REFERENCES spieler(id),
+    zugelassen BOOLEAN NOT NULL,
+    qualifying INTEGER NOT NULL REFERENCES qualifying(id)
+);
+
+CREATE TABLE qualifyingspieler (
+    id INTEGER NOT NULL REFERENCES qualifying(id),
+    spielerid INTEGER NOT NULL REFERENCES spieler(id)
+);
