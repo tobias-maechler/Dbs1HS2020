c13ec0fb715b624ac25a051bbef9a8dcba1db7c6
Kevin Loeffler <hi@kevinloeffler.ch>
2020-11-23 15:00:13 +0100

    Run script 4 'constraints'

--- Testat3/0_runAllScripts.sql
+++ Testat3/0_runAllScripts.sql
@@ -44,7 +44,7 @@ CREATE DATABASE :database WITH OWNER :user ENCODING 'UTF8';
 \ir 3_inserts.sql
 
 -- create primary keys, constraints, indexes
---\ir 4_constraints.sql
+\ir 4_constraints.sql
 
 \echo
 \prompt 'Execute Queries (\\q or Ctrl-C to abort)?' promptvar
